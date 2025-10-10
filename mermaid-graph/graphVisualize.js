9c6bacd3fbb9cef09c69d513fe8304c5126ff486
Vinayaka Swamy H M <86816121+VinZCodz@users.noreply.github.com>
2025-10-10 08:49:07 +0000

    Flow Diagram Ready.

--- mermaid-graph/graphVisualize.js
+++ mermaid-graph/graphVisualize.js
@@ -6,15 +6,26 @@ import { AnalyzerAgent } from "../src/backend/analyzer/graph.ts";
 import { WriterAgent } from "../src/backend/writer/graph.ts";
 import { FormatterAgent } from "../src/backend/formatter/graph.ts";
 
-for async (const agent of [[JotterAgent, "converse"], [SupervisorAgent, "supervisor"], [ResearchAgent, "researcher"], [AnalyzerAgent, "analyzer"], [WriterAgent, "writer"], [FormatterAgent, "formatter"]]) {
-    await graphVisualize(agent, );
-}
-
 const graphVisualize = async (agent, graphName) => {
     const drawableGraphGraphState = await agent.getGraphAsync();
     const graphStateImage = await drawableGraphGraphState.drawMermaidPng();
     const graphStateArrayBuffer = await graphStateImage.arrayBuffer();
 
-    const filePath = `./${graphName}_flow.png`;
+    const filePath = `./mermaid-graph/${graphName}_flow.png`;
     await fs.writeFile(filePath, new Uint8Array(graphStateArrayBuffer));
+};
+
+const main = async () => {
+    try {
+        for (const agent of [
+            [JotterAgent, "converse"], [SupervisorAgent, "supervisor"],
+            [ResearchAgent, "researcher"], [AnalyzerAgent, "analyzer"],
+            [WriterAgent, "writer"], [FormatterAgent, "formatter"]
+        ]) await graphVisualize(agent[0], agent[1]);
+
+    } finally {
+        console.warn(`\n Flow Diagram Ready!\n`);
+    };
 }
+
+await main();
